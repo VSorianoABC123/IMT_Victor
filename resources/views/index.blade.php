e8780da6891b9efceb14c858734baad76b5a072c
VSorianoABC123 <victorsorianocalderon@gmail.com>
2022-05-12 13:59:54 +0200

    Pujat Projecte Inicial

---
+++ resources/views/index.blade.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.blade.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">Navbar</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Dropdown link
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                      <li><a class="dropdown-item" href="#">Action</a></li>
+                      <li><a class="dropdown-item" href="#">Another action</a></li>
+                      <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+
+    </header>
+
+    <main class="row">
+
+
+
+    <aside id="sidebar" class="col-1" >
+    <div>
+
+        <button id="tancarSidebar" class="btn btn-primary active m-1 position-relative start-50" aria-current="page"><</button>
+        <ul class="nav nav-pills flex-column mb-auto">
+          <li class="nav-item">
+            <a href="#" class="nav-link active" aria-current="page">
+              Sobre mi
+            </a>
+          </li>
+          <li>
+            <a href="#" class="nav-link link-dark">
+              Els meus projectes
+            </a>
+          </li>
+          <li>
+            <a href="#" class="nav-link link-dark">
+              Altres habilitats
+            </a>
+          </li>
+          <li>
+            <a href="#" class="nav-link link-dark">
+              Contacte
+            </a>
+          </li>
+        </ul>
+      </div>
+
+    </aside>
+
+    <section class="col-11">
+        <!-----------------CARROUSEL---------------->
+        <h1 class="offset-md-5">HOLA</h1>
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="{{url('img/Captura1.png')}}" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="{{url('img/Captura2.png')}}" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="{{url('img/Captura3.png')}}" class="d-block w-100" alt="...">
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+
+        <div class= "mt-5">
+            absagikbagksajgbsakgsabaskjgasbksabga
+
+        </div>
+        <div class= "mt-5">
+            absagikbagksajgbsakgsabaskjgasbksabga
+
+        </div>
+        <div class= "mt-5">
+            absagikbagksajgbsakgsabaskjgasbksabga
+
+        </div>
+        <div class= "mt-5">
+            absagikbagksajgbsakgsabaskjgasbksabga
+
+        </div>
+        <div class= "mt-5">
+            absagikbagksajgbsakgsabaskjgasbksabga
+
+        </div>
+        <div class= "mt-5">
+            absagikbagksajgbsakgsabaskjgasbksabga
+
+        </div>
+        <div class= "mt-5">
+            absagikbagksajgbsakgsabaskjgasbksabga
+
+        </div>
+        <div class= "mt-5">
+            absagikbagksajgbsakgsabaskjgasbksabga
+
+        </div>
+        <div class= "mt-5">
+            absagikbagksajgbsakgsabaskjgasbksabga
+
+        </div>
+        <div class= "mt-5">
+            absagikbagksajgbsakgsabaskjgasbksabga
+
+        </div>
+        <div class= "mt-5">
+            absagikbagksajgbsakgsabaskjgasbksabga
+
+        </div>
+        <div class= "mt-5">
+            absagikbagksajgbsakgsabaskjgasbksabga
+
+        </div>
+        <div class= "mt-5">
+            absagikbagksajgbsakgsabaskjgasbksabga
+
+        </div>
+        <div class= "mt-5">
+            absagikbagksajgbsakgsabaskjgasbksabga
+
+        </div>
+        <div class= "mt-5">
+            absagikbagksajgbsakgsabaskjgasbksabga
+
+        </div>
+        <div class= "mt-5">
+            absagikbagksajgbsakgsabaskjgasbksabga
+
+        </div>
+        <div class= "mt-5">
+            absagikbagksajgbsakgsabaskjgasbksabga
+
+        </div>
+        <div class= "mt-5">
+            absagikbagksajgbsakgsabaskjgasbksabga
+
+        </div>
+        <div class= "mt-5">
+            absagikbagksajgbsakgsabaskjgasbksabga
+
+        </div>
+        <div class= "mt-5">
+            absagikbagksajgbsakgsabaskjgasbksabga
+
+        </div>
+        <div class= "mt-5">
+            absagikbagksajgbsakgsabaskjgasbksabga
+
+        </div>
+
+
+    </section>
+
+    </main>
+
+    </body>
+
+
+    <script src="/docs/5.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <script src="sidebars.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+</script>
+
+    <script>
+
+
+        let boto = document.getElementById("tancarSidebar");
+
+        boto.addEventListener("click",function(){
+
+
+            document.getElementById("sidebar").classList.add("invisible");
+
+            document.getElementById("sidebar").classList.remove("col-3");
+            document.getElementById("sidebar").classList.add("col-0");
+
+
+        }, false);
+
+
+    </script>
+</html>
